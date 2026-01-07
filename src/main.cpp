d678c5486704981f07d68c285bf72a0d6deddf45
J Dipayan Rao <raodipayan@gmail.com>
2026-01-07 22:32:54 +0530

    main.cpp updated language phase

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,81 @@
-#include<iostream>
-#include<string>
-#include<algorithm>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include "parser.h"
+
 using namespace std;
-int main(){
-    cout<<"Welcome to KittyDB"<<endl;
-    cout<<"Type 'exit' to quit"<<endl;
-    
-    while(true){
-        cout<<"KittyDB > ";
+int main()
+{
+    cout << "Welcome to KittyDB" << endl;
+    cout << "Type 'exit' to quit" << endl;
+
+    while (true)
+    {
+        cout << "KittyDB > ";
         string command;
         getline(cin, command);
 
-        if(command.empty()){
+        if (command.empty())
+        {
             continue;
         }
 
         string cmdUpper = command;
         transform(cmdUpper.begin(), cmdUpper.end(), cmdUpper.begin(), ::toupper);
 
-
-        if(cmdUpper == "EXIT"){
+        if (cmdUpper == "EXIT")
+        {
             break;
         }
-        else if(cmdUpper.rfind("CREATE TABLE", 0)==0){
-            cout<<"CREATE TABLE command detected"<<endl;
-        }
-        else if (cmdUpper.rfind("INSERT INTO", 0) == 0) {
-            cout << "INSERT command detected" << endl;
-        }
-        else if (cmdUpper.rfind("SELECT", 0) == 0) {
-            cout << "SELECT command detected" << endl;
-        }
-        else if (cmdUpper.rfind("DELETE", 0) == 0) {
-            cout << "DELETE command detected" << endl;
-        }
-        else if (cmdUpper.rfind("UPDATE", 0) == 0) {
-            cout << "UPDATE command detected" << endl;
-        }
-        else {
+
+        ParsedCommand pc = parseCommand(command);
+
+        switch (pc.type)
+        {
+        case CommandType::CREATE_DATABASE:
+            cout << "CREATE DATABASE\n";
+            cout << "Database: " << pc.databaseName << endl;
+            break;
+
+        case CommandType::USE_DATABASE:
+            cout << "USE DATABASE\n";
+            cout << "Database: " << pc.databaseName << endl;
+            break;
+
+        case CommandType::CREATE:
+            cout << "CREATE TABLE\n";
+            cout << "Table: " << pc.tableName << endl;
+            cout << "Schema: " << pc.schema << endl;
+            break;
+
+        case CommandType::INSERT:
+            cout << "INSERT\n";
+            cout << "Table: " << pc.tableName << endl;
+            cout << "Values: " << pc.values << endl;
+            break;
+
+        case CommandType::SELECT:
+            cout << "SELECT\n";
+            cout << "Table: " << pc.tableName << endl;
+            cout << "Where: " << pc.whereClause << endl;
+            break;
+
+        case CommandType::DELETE_CMD:
+            cout << "DELETE\n";
+            cout << "Table: " << pc.tableName << endl;
+            cout << "Where: " << pc.whereClause << endl;
+            break;
+
+        case CommandType::UPDATE:
+            cout << "UPDATE\n";
+            cout << "Table: " << pc.tableName << endl;
+            cout << "Set: " << pc.setClause << endl;
+            cout << "Where: " << pc.whereClause << endl;
+            break;
+
+        default:
             cout << "Unknown command" << endl;
         }
     }
-    cout<<"Exiting KittyDB..."<<endl;
+    cout << "Exiting KittyDB..." << endl;
 }
