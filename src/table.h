80e6b40850504e93ae1b2bed4191f41df8291daf
J Dipayan Rao <raodipayan@gmail.com>
2026-01-08 20:33:16 +0530

    table.h created

---
+++ src/table.h
@@ -0,0 +1,13 @@
+#ifndef TABLE_H
+#define TABLE_H
+#include<string>
+
+//create table files in database
+bool createTable(
+    const std::string& databaseName,
+    const std::string& tableName,
+    const std::string& schema,
+    std::string& error 
+);
+
+#endif
