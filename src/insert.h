15f7dfff089a6540dd469edfd08d441a1436cd58
J Dipayan Rao <raodipayan@gmail.com>
2026-01-08 22:43:03 +0530

    insert.h created

---
+++ src/insert.h
@@ -0,0 +1,13 @@
+#ifndef INSERT_H
+#define INSERT_H
+
+#include <string>
+
+bool insertRow(
+    const std::string& databaseName,
+    const std::string& tableName,
+    const std::string& values,
+    std::string& error
+);
+
+#endif
