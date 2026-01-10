827c71220e63302f11940509ecefaee8a067510d
J Dipayan Rao <raodipayan@gmail.com>
2026-01-10 22:26:36 +0530

    update.h created

---
+++ src/update.h
@@ -0,0 +1,16 @@
+#ifndef UPDATE_H
+#define UPDATE_H
+
+#include <string>
+#include "parser.h"   
+
+bool updateWhere(
+    const std::string& databaseName,
+    const std::string& tableName,
+    const std::string& setClause,
+    bool hasWhere,
+    const WhereCondition& where, //parser.h
+    std::string& error
+);
+
+#endif
