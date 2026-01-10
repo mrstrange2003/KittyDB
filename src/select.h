8d56ccdbbd279bd2ca543dbd5ce436c1f1747efc
J Dipayan Rao <raodipayan@gmail.com>
2026-01-10 21:01:18 +0530

    select.h updated WHERE command implemented

--- src/select.h
+++ src/select.h
@@ -1,14 +1,17 @@
 #ifndef SELECT_H
 #define SELECT_H
-#include<string>
-#include<vector>
+
+#include <string>
+#include <vector>
+#include "parser.h"   // for WhereCondition
 
 bool selectColumns(
     const std::string& databaseName,
     const std::string& tableName,
     const std::vector<std::string>& selectedColumns,
+    bool hasWhere,
+    const WhereCondition& where,
     std::string& error
 );
-
 
 #endif
