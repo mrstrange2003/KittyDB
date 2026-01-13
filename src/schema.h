d8ff371fe26a7e29628be4e8af44e624896314bf
J Dipayan Rao <raodipayan@gmail.com>
2026-01-13 22:26:10 +0530

    schema.h updated

--- src/schema.h
+++ src/schema.h
@@ -1,14 +1,15 @@
+// schema.h
 #ifndef SCHEMA_H
 #define SCHEMA_H
 #include<string>
 #include<vector>
 
-struct Column{
+struct Column {
     std::string name;
     std::string type;
 };
 
-//parse schema from .meta file
+// parse schema from .meta file
 bool parseSchema(
     const std::string& metaPath,
     std::vector<Column>& columns,
