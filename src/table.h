16ba4729f00200f02597095f3acd254c56aaf0fb
J Dipayan Rao <raodipayan@gmail.com>
2026-01-22 22:29:21 +0530

    finishing touches done

--- src/table.h
+++ src/table.h
@@ -1,30 +1,27 @@
 #ifndef TABLE_H
 #define TABLE_H
-#include<string>
 
-//create table files in database
+#include <string>
+
+// create table files in database
 bool createTable(
-    const std::string& databaseName,
-    const std::string& tableName,
-    const std::string& schema,
-    std::string& error 
-);
+    const std::string &databaseName,
+    const std::string &tableName,
+    const std::string &schema,
+    std::string &error);
 
 bool describeTable(
-    const std::string& databaseName,
-    const std::string& tableName,
-    std::string& error
-);
+    const std::string &databaseName,
+    const std::string &tableName,
+    std::string &error);
 
 bool showTables(
-    const std::string& databaseName,
-    std::string& error
-);
+    const std::string &databaseName,
+    std::string &error);
 
 bool truncateTable(
-    const std::string& databaseName,
-    const std::string& tableName,
-    std::string& error
-);
+    const std::string &databaseName,
+    const std::string &tableName,
+    std::string &error);
 
 #endif
