b00b829b2aa41ffd8f9c152ab21efc74e6f9f64f
J Dipayan Rao <raodipayan@gmail.com>
2026-01-22 22:26:57 +0530

    finishing touches done

--- src/db.h
+++ src/db.h
@@ -1,8 +1,9 @@
 #ifndef DB_H
 #define DB_H
-#include<string>
 
-//creates database  dorectory 
-bool createDatabase(const std::string& dbName, std::string& error);
+#include <string>
+
+// creates database  dorectory
+bool createDatabase(const std::string &dbName, std::string &error);
 
 #endif
