5cfd5d07739eb2fc7dd726fc4c17927ea748e056
J Dipayan Rao <raodipayan@gmail.com>
2026-01-22 22:29:58 +0530

    finishing touches done

--- src/update.h
+++ src/update.h
@@ -1,17 +1,18 @@
 // update.h
+
 #ifndef UPDATE_H
 #define UPDATE_H
 
-#include <string>
 #include "parser.h"
 
+#include <string>
+
 bool updateWhere(
-    const std::string& databaseName,
-    const std::string& tableName,
-    const std::string& setClause,
+    const std::string &databaseName,
+    const std::string &tableName,
+    const std::string &setClause,
     bool hasWhere,
-    const WhereCondition& where,
-    std::string& error
-);
+    const WhereCondition &where,
+    std::string &error);
 
 #endif
