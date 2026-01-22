6e0625e5c2d6ad9414a2312cbf1090297d62357e
J Dipayan Rao <raodipayan@gmail.com>
2026-01-22 22:27:47 +0530

    finishing touches done

--- src/insert.h
+++ src/insert.h
@@ -1,14 +1,14 @@
 // insert.h
+
 #ifndef INSERT_H
 #define INSERT_H
 
 #include <string>
 
 bool insertRow(
-    const std::string& databaseName,
-    const std::string& tableName,
-    const std::string& values,
-    std::string& error
-);
+    const std::string &databaseName,
+    const std::string &tableName,
+    const std::string &values,
+    std::string &error);
 
 #endif
