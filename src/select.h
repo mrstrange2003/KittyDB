b23cda266a8ecbde6a3f261530d360239970ce59
J Dipayan Rao <raodipayan@gmail.com>
2026-01-22 22:28:51 +0530

    finishing touches done

--- src/select.h
+++ src/select.h
@@ -1,28 +1,29 @@
 // select.h
+
 #ifndef SELECT_H
 #define SELECT_H
 
-#include <string>
-#include <vector>
 #include "parser.h"
 #include "where.h"
 #include "aggregate.h"
 
+#include <string>
+#include <vector>
+
 bool selectColumns(
-    const std::string& databaseName,
-    const std::string& tableName,
-    const std::vector<std::string>& selectedColumns,
+    const std::string &databaseName,
+    const std::string &tableName,
+    const std::vector<std::string> &selectedColumns,
     bool hasWhere,
-    const WhereCondition& where,
+    const WhereCondition &where,
     bool distinct,
     bool hasOrderBy,
-    const OrderByClause& orderBy,
+    const OrderByClause &orderBy,
     bool hasLimit,
     int limitCount,
     int offsetCount,
     bool hasAggregates,
-    const std::vector<AggregateFunction>& aggregates,
-    std::string& error
-);
+    const std::vector<AggregateFunction> &aggregates,
+    std::string &error);
 
 #endif
